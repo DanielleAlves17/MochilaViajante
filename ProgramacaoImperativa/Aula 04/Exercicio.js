50ba6e3fd137923f1a5b8822058a3540bf707fa5
DanielleAlves17 <89142483+DanielleAlves17@users.noreply.github.com>
2021-12-06 14:38:18 -0300

    Add files via upload

---
+++ ProgramacaoImperativa/Aula 04/Exercicio.js
@@ -0,0 +1,11 @@
+console.log("Exercicio");
+
+let Nome = "Carlos de Souza";
+var Idade = 28;
+var Peso = 80.1;
+var Altura = 1.76;
+let Plano = "true";
+let IMC = Peso/(Altura*Altura);
+
+
+console.log (Nome + ' tem ' + Idade + ' anos e seu índice de massa corporal é ' + IMC);
